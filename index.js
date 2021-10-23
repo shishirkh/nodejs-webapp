6619fba276b5019fc5c8c3c213af9cb292535ae3
shishirkh <shishir29121997@gmail.com>
2021-10-23 21:20:36 +0530

    add nodejs app

---
+++ index.js
@@ -0,0 +1,21 @@
+const dotenv=require('dotenv'); 
+dotenv.config({ path: './env' });
+
+dotenv.config();
+
+const express=require("express");
+const app=express();
+
+app.get('/',(req,res)=>{
+  res.send(`This is a sample nodejs webapp created by Shishir! IP address of the server is ${process.env.IP_ADDRESS}`);
+});
+
+
+app.listen(process.env.PORT,(err)=>{
+    if(err){
+        console.log(`Error: ${err.message}`);
+    }else{
+        console.log(`Listening on port ${process.env.PORT}`);
+    }
+  }
+)
